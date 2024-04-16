1fb86cb7fce98660123ca0c13f855dc46a732ed4
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-16 15:37:24 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+def finalizar_app():
+  os.system('cls')
+  print('Finalizando o app\n')
+  
+
+
+# print(f'Você escolheu a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+  print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+  print('Listar restaurante')
+elif opcao_escolhida == 3:
+  print('Ativar restaurante')
+else:
+  finalizar_app()
+
